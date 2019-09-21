92e11beb094801a27bb4744e1f415514a21ba145
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-21 14:58:20 +0900

    add projects#index spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+	describe "#index" do
+		let!(:user) { create(:user)}
+
+	  it 'returns a 200 response' do
+			sign_in user
+	    get :index
+			expect(response).to have_http_status 200
+	  end
+	end
 
 end

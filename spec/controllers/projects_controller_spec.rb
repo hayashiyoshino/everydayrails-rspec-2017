fa7c57adb7924cc6765abaf81fc4bb66614a0fa8
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-21 15:24:11 +0900

    add projec#show spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -24,4 +24,30 @@ RSpec.describe ProjectsController, type: :controller do
 			end
 		end
 	end
+
+	describe '#show' do
+
+		context 'authenticated user' do
+			let!(:user) { create(:user) }
+			let!(:project) { create(:project, owner: user) }
+
+			it 'returns a 200 response' do
+				sign_in user
+				get :show, params: { id: project.id }
+				expect(response).to have_http_status 200
+			end
+		end
+
+		# context 'as a guest' do
+		# 	it 'returns a 302 response' do
+		# 		get :index
+		# 		expect(response).to have_http_status 302
+		# 	end
+		#
+		# 	it 'redirects to the sign in page' do
+		# 		get :index
+		# 		expect(response).to redirect_to '/users/sign_in'
+		# 	end
+		# end
+	end
 end

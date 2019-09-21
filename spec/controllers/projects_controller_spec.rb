d06601664bc798fe943daa95c23937cc80a6a9b1
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-21 15:11:27 +0900

    as a guest / project_controller_spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
-	describe "#index" do
-		let!(:user) { create(:user)}
+	describe '#index' do
+		let!(:user) { create(:user) }
 
 	  it 'returns a 200 response' do
 			sign_in user
 	    get :index
 			expect(response).to have_http_status 200
-	  end
-	end
+		end
 
+		context 'as a guest' do
+		  it 'returns a 302 response' do
+		    get :index
+				expect(response).to have_http_status 302
+			end
+
+			it 'redirects to the sign in page' do
+			  get :index
+				expect(response).to redirect_to '/users/sign_in'
+			end
+		end
+	end
 end

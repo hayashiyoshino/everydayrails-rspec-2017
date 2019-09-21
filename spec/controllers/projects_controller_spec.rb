027b9f04683468a60e52a052dfac5e4a26cc48d9
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-21 15:16:59 +0900

    add context 'authenticated user'

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
 	describe '#index' do
-		let!(:user) { create(:user) }
 
-	  it 'returns a 200 response' do
-			sign_in user
-	    get :index
-			expect(response).to have_http_status 200
-		end
+	  context 'authenticated user' do
+			let!(:user) { create(:user) }
+			it 'returns a 200 response' do
+				sign_in user
+				get :index
+				expect(response).to have_http_status 200
+			end
+	  end
 
 		context 'as a guest' do
 		  it 'returns a 302 response' do
